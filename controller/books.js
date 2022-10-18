9f8385b8084d20ca2d853ccb45e65210f714f74f
Alex Schmidt <alex.schmidt767@gmail.com>
2022-10-17 23:02:13 -0500

    finished assignment

--- controller/books.js
+++ controller/books.js
@@ -1,54 +1,54 @@
 const router = require('express').Router();
 const db = require('../models');
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
     db.Book.find()
         .then( books => res.status(200).json(books) )
         .catch( err => {
             console.log('err', err);
-            res.status(404).render('error404');
+            res.status(404).json({ message: 'error404' });
           });
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', (req, res, next) => {
     db.Book.findById(req.params.id)
         .then( book => res.status(200).json(book) )
         .catch( err => {
             console.log('err', err);
-            res.status(404).render('error404');
+            res.status(404).json({ message: 'error404' });
           });
 });
 
-router.post('/', (req, res) => {
+router.post('/', (req, res, next) => {
     db.Book.create(req.body)
         .then( () => res.status(201).json({
                 message: 'Book created successfully!'
         }))
         .catch( err => {
             console.log('err', err);
-            res.status(404).render('error404');
+            res.status(404).json({ message: 'error404' });
           });
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', (req, res, next) => {
     db.Book.findByIdAndUpdate(req.params.id, req.body)
         .then( () => res.status(200).json({
             message: 'Book updated successfully!'
         }))
         .catch( err => {
             console.log('err', err);
-            res.status(404).render('error404');
+            res.status(404).json({ message: 'error404' });
           });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', (req, res, next) => {
     db.Book.findByIdAndDelete(req.params.id)
     .then( () => res.status(200).json({
         message: 'Book deleted successfully!'
     }))
     .catch( err => {
         console.log('err', err);
-        res.status(404).render('error404');
+        res.status(404).json({ message: 'error404' });
       });
 })
 
--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/books', require('./controller/books'));
 
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
     res.send('Hello World!');
 });
 
-app.get('*', (req, res) => {
+app.get('*', (req, res, next) => {
     res.status(404).send('ERROR 404 PAGE NOT FOUND');
 });
 
