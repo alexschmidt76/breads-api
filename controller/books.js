11e8e2b9d5d617c8157769308508f4d3e59ca6f3
Alex Schmidt <alex.schmidt767@gmail.com>
2022-10-18 18:08:37 -0500

    updated catch status codes and messages

--- controller/books.js
+++ controller/books.js
@@ -1,55 +1,39 @@
 const router = require('express').Router();
 const db = require('../models');
 
+// GET /books
 router.get('/', (req, res, next) => {
     db.Book.find()
         .then( books => res.status(200).json(books) )
-        .catch( err => {
-            console.log('err', err);
-            res.status(404).json({ message: 'error404' });
-          });
+        .catch( err => res.status(404).json({ message: `Error 404: Page Not Found\nError Message:\n${err}` }) );
 });
 
+// GET /books/:id
 router.get('/:id', (req, res, next) => {
     db.Book.findById(req.params.id)
         .then( book => res.status(200).json(book) )
-        .catch( err => {
-            console.log('err', err);
-            res.status(404).json({ message: 'error404' });
-          });
+        .catch( err => res.status(404).json({ message: `Error 404: Page Not Found\nError Message:\n${err}` }) );
 });
 
+// POST /books
 router.post('/', (req, res, next) => {
     db.Book.create(req.body)
-        .then( () => res.status(201).json({
-                message: 'Book created successfully!'
-        }))
-        .catch( err => {
-            console.log('err', err);
-            res.status(404).json({ message: 'error404' });
-          });
+        .then( () => res.status(201).json({ message: 'Book created successfully!' }))
+        .catch( err => res.status(400).json({ message: `Error 400: Bad Request\nError Message:\n${err}` }) );
 });
 
+// PUT /books/:id
 router.put('/:id', (req, res, next) => {
     db.Book.findByIdAndUpdate(req.params.id, req.body)
-        .then( () => res.status(200).json({
-            message: 'Book updated successfully!'
-        }))
-        .catch( err => {
-            console.log('err', err);
-            res.status(404).json({ message: 'error404' });
-          });
+        .then( () => res.status(200).json({ message: 'Book updated successfully!' }) )
+        .catch( err => res.status(400).json({ message: `Error 400: Bad Request\nError Message:\n${err}` }) );
 });
 
+// DELETE /books/:id
 router.delete('/:id', (req, res, next) => {
     db.Book.findByIdAndDelete(req.params.id)
-    .then( () => res.status(200).json({
-        message: 'Book deleted successfully!'
-    }))
-    .catch( err => {
-        console.log('err', err);
-        res.status(404).json({ message: 'error404' });
-      });
-})
+    .then( () => res.status(200).json({ message: 'Book deleted successfully!' }) )
+    .catch( err => res.status(400).json({ message: `Error 400: Bad Request\nError Message:\n${err}` }) );
+});
 
 module.exports = router;
